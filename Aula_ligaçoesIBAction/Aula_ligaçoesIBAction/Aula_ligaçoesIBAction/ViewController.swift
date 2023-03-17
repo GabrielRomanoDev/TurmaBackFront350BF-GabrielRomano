58825d412211db100ec4fa1e6cf9aecef8bbcf12
Gabriel Romano <gromanoluz@gmail.com>
2023-03-16 22:55:38 -0300

    Criada a  Aplicação

---
+++ Aula_ligaçoesIBAction/Aula_ligaçoesIBAction/Aula_ligaçoesIBAction/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Aula_ligaçoesIBAction
+//
+//  Created by Gabriel Luz Romano on 16/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
